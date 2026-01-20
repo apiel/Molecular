5b8224e2090eff1f3797ae1fedddb00b07c8ecf9
apiel <alexandre.piel@gmail.com>
2026-01-20 20:32:19 +0100

    build into doc folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +8,7 @@ export default defineConfig({
   // Use relative base path so it works on any GitHub Pages URL (e.g. username.github.io/repo-name/)
   base: './',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     sourcemap: true
   }
 });
